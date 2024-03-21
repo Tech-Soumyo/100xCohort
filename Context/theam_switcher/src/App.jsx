52d92cc31d887c349dc90393b8d485f93c388f3d
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-03-21 22:23:22 +0530

    tailwind mode configure, Thanks to Hitesh Sir

--- Context/theam_switcher/src/App.jsx
+++ Context/theam_switcher/src/App.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { ThemeProvider } from "./context/theme";
-
+import Card from "./components/Card";
+import ThemeBtn from "./components/ThemeBtn";
 function App() {
   const [themeMode, setThemeMode] = useState("light");
 
   const lightTheme = () => {
     setThemeMode("light");
   };
-  const darkThemeTheme = () => {
+  const darkTheme = () => {
     setThemeMode("dark");
   };
 
@@ -22,10 +23,12 @@ function App() {
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {/* themebtn */}
+            <ThemeBtn />
           </div>
 
-          <div className="w-full max-w-sm mx-auto">{/* card */}</div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
         </div>
       </div>
     </ThemeProvider>
