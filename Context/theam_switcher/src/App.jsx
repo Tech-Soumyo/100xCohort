ef2f8468c504c6ca4da000e5f5a64bbfd19feafc
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-03-21 21:43:58 +0530

    funtionalities Done

--- Context/theam_switcher/src/App.jsx
+++ Context/theam_switcher/src/App.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { ThemeProvider } from "./context/theme";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkThemeTheme = () => {
+    setThemeMode("dark");
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
   return (
-    <>
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
@@ -12,7 +28,7 @@ function App() {
           <div className="w-full max-w-sm mx-auto">{/* card */}</div>
         </div>
       </div>
-    </>
+    </ThemeProvider>
   );
 }
 
