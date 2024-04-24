c9927cfc086a1ff96c19fe5a5a2629b6deb86456
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-04-25 03:16:09 +0530

    useIsOnline hook

--- Custom Hooks/hooks/src/App.jsx
+++ Custom Hooks/hooks/src/App.jsx
@@ -81,63 +81,83 @@ import "./App.css";
 // export default App;
 
 //  Custom Hooks
-function useTodos(n) {
-  const [todos, setTodos] = useState([]);
-  const [loading, setLoading] = useState(true);
+// function useTodos(n) {
+//   const [todos, setTodos] = useState([]);
+//   const [loading, setLoading] = useState(true);
 
+//   useEffect(() => {
+//     const value = setInterval(() => {
+//       axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
+//         setTodos(res.data.todos);
+//         setLoading(false);
+//       });
+//     }, [n * 1000]);
+//     axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
+//       setTodos(res.data.todos);
+//       setLoading(false);
+//     });
+
+//     return () => {
+//       clearInterval(value);
+//     };
+//   }, [n]);
+//   return { todos, loading };
+// }
+
+// function App() {
+//   const { todos, loading } = useTodos(5); // using custom hooks
+
+//   //  const [todos, setTodos] = useState([]);   // using useState hook normally
+
+//   //  useEffect(() => {
+//   //    axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
+//   //      setTodos(res.data.todos);
+//   //    });
+//   //  }, []);
+//   if (loading) {
+//     return (
+//       <>
+//         <div>loading...</div>
+//       </>
+//     );
+//   }
+
+//   return (
+//     <>
+//       {todos.map((todo) => (
+//         <Track todo={todo} />
+//       ))}
+//     </>
+//   );
+// }
+
+// function Track({ todo }) {
+//   return (
+//     <div>
+//       {todo.title}
+//       <br />
+//       {todo.description}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+const useIsOnline = () => {
+  const [isonline, setIsOnline] = useState(window.navigator.onLine);
   useEffect(() => {
-    const value = setInterval(() => {
-      axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
-        setTodos(res.data.todos);
-        setLoading(false);
-      });
-    }, [n * 1000]);
-    axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
-      setTodos(res.data.todos);
-      setLoading(false);
-    });
-
-    return () => {
-      clearInterval(value);
-    };
-  }, [n]);
-  return { todos, loading };
-}
+    window.addEventListener("online", () => setIsOnline(true));
+    window.addEventListener("offline", () => setIsOnline(false));
+  }, []);
+  return isonline;
+};
 
 function App() {
-  const { todos, loading } = useTodos(5); // using custom hooks
-
-  //  const [todos, setTodos] = useState([]);   // using useState hook normally
-
-  //  useEffect(() => {
-  //    axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
-  //      setTodos(res.data.todos);
-  //    });
-  //  }, []);
-  if (loading) {
-    return (
-      <>
-        <div>loading...</div>
-      </>
-    );
-  }
-
+  const isonline = useIsOnline();
   return (
     <>
-      {todos.map((todo) => (
-        <Track todo={todo} />
-      ))}
+      <div>{isonline ? "user Is Online" : "user Is offline"}</div>
     </>
-  );
-}
-
-function Track({ todo }) {
-  return (
-    <div>
-      {todo.title}
-      <br />
-      {todo.description}
-    </div>
   );
 }
 
