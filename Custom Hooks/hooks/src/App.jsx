6b5942d52776c78c402b5f0cdd00ee941181dc27
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-04-24 13:40:35 +0530

    funtional hooks

--- Custom Hooks/hooks/src/App.jsx
+++ Custom Hooks/hooks/src/App.jsx
@@ -28,25 +28,4 @@ function MyFuntionalComponent() {
   );
 }
 
-class MyClassicalComponents extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
-  }
-
-  incrementCount = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  render() {
-    return (
-      <div>
-        <p>{this.state.count}</p>
-        <button onClick={this.incrementCount}>
-          ClassicalComponentsIncrement
-        </button>
-      </div>
-    );
-  }
-}
 export default App;
