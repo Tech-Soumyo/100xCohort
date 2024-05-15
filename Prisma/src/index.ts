f2efea5717bc4104ec2ecc4865c39af0a8b0a9c2
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-05-16 02:38:55 +0530

    generate the client for migration

---
+++ Prisma/src/index.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function insertUser(
+  username: string,
+  password: string,
+  firstName: string,
+  lastName: string
+) {
+  const res = await prisma.user.create({
+    data: {
+      email: username,
+      passWord: password,
+      firstName,
+      lastName,
+    },
+  });
+  console.log(res);
+}
+
+insertUser("admin1", "123456", "harkirat", "singh");
