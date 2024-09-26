a82277e4902f44b828380d5ccd122fe37f5ed1d2
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-09-26 12:38:34 +0530

    Record

---
+++ TypeScript/advanced api/dist/record.js
@@ -0,0 +1,15 @@
+"use strict";
+const users = {
+    ghjdhfs: {
+        id: "1",
+        userName: "soumyo",
+    },
+    nuguihj: {
+        id: "2",
+        userName: "Tech",
+    },
+};
+const users0 = {
+    dfkjkldfjd: 255,
+    fkldfjdkfjd: 256,
+};
