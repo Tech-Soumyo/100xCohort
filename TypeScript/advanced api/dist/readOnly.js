791a4c483af2813f191ef630205ebe38a121fe18
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-09-26 12:39:07 +0530

    ReadOnly

---
+++ TypeScript/advanced api/dist/readOnly.js
@@ -0,0 +1,6 @@
+"use strict";
+const config = {
+    endPoint: "https://api.example.com",
+    apiKey: "abcdef123456",
+};
+// config.apiKey = 'newkey'; // Error: Cannot assign to 'apiKey' because it is a r
