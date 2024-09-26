c136fe582c5e2633147337e2cd0642d0fd494d65
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-09-26 12:39:31 +0530

    pick

---
+++ TypeScript/advanced api/dist/pick.js
@@ -0,0 +1,11 @@
+"use strict";
+// interface User {
+//   age: number;
+//   Name: string;
+// }
+function updateUser(updatedProps) { }
+const user = updateUserPartial({
+    Name: "fdd",
+    age: 45,
+    password: "755",
+});
