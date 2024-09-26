b296148ea559b4921461f8bebddc72925db3baa5
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-09-26 12:37:52 +0530

    z.infer()

---
+++ TypeScript/advanced api/src/ZodSchema.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+import express from "express";
+
+const app = express();
+
+// Define the schema for profile update
+const userProfileSchema = z.object({
+  name: z.string().min(1, { message: "Name cannot be empty" }),
+  email: z.string().email({ message: "Invalid email format" }),
+  age: z
+    .number()
+    .min(18, { message: "You must be at least 18 years old" })
+    .optional(),
+});
+
+export type FinalUserSchema = z.infer<typeof userProfileSchema>;
+
+app.put("/user", (req, res) => {
+  const { success } = userProfileSchema.safeParse(req.body);
+  const updateBody: FinalUserSchema = req.body; // how to assign a type to updateBody?
+
+  if (!success) {
+    res.status(411).json({});
+    return;
+  }
+  // update database here
+  res.json({
+    message: "User updated",
+  });
+});
+
+// app.listen(3000);
+
+// const StringZodSchema = z.string();
+
+// type StringZodType = z.infer<typeof StringZodSchema>;
